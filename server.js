3aa7e2e54bedeed2cd835c0a772a48c839673812
mario-viamonte <dsv3634@gmail.com>
2022-02-22 01:11:47 -0400

    Update Crud_diflen

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+const MongoClient = require('mongodb').MongoClient 
+
+
+const uri = "mongodb+srv://playviamonte:playdev21@cluster0.9gxda.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+MongoClient.connect(uri, (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('Crud_Diflen')
+
+  app.listen(3009, function(){
+    console.log("esta aplicação está rodando na porta 3009")
+  })  
+})
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -12,10 +24,12 @@ app.get('/', (req,res)=> {
 })
 
 app.post('/show', (req,res) => {
-    console.log(req.body)
+    db.collection('data').save(req.body, (err, result) =>{
+        if (err) return console.log(rserr)
+
+        console.log("salvo no BD")
+        res.redirect('/')
+    })
 })
 
-app.listen(3000, function(){
-    console.log("esta aplicação está rodando na porta 3000")
-})
     
